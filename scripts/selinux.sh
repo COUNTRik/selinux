ca7692cbb6393236abc4f9aab79867babdcf73be
Albert <countrik@yandex.ru>
2020-10-08 16:58:18 +0300

    update scripts

--- scripts/selinux.sh
+++ scripts/selinux.sh
@@ -5,5 +5,15 @@ mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 
 # Установим необходимые пакеты
-yum install epel-release
+yum install -y epel-release
 yum install -y mc vim nginx setroubleshoot-server setools-console policycoreutils-python policycoreutils-newrole selinux-policy-mls
+
+# Изменим порт nginx в конфигурационном файле
+sed -i 's/80/3200/g' /etc/nginx/nginx.conf
+
+# Добавим нужный нам порт в http_port_t
+semanage port -a -t http_port_t -p tcp 3200
+
+# Запустим nginx
+systemctl start nginx
+systemctl enable nginx
